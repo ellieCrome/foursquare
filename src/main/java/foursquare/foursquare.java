ed58525ecc24b2ddafbe5b2ee132c386fc3ea1f6
Ellie Crome <ecrome@andigital.com>
2017-01-16 19:19:14 +0000

    added unit test

--- src/main/java/foursquare/foursquare.java
+++ src/main/java/foursquare/foursquare.java
@@ -3,7 +3,7 @@ package foursquare;
 /**
  * Created by ecrome on 16/01/2017.
  */
-public class foursquare {
+public class Foursquare {
 
     public static void main(String[] args) {
         System.out.println("hello world");
---
+++ src/test/unit/foursquare/FoursquareTest.java
@@ -0,0 +1,24 @@
+package foursquare;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Tests for {@link foursquare}
+ *
+ * @author Ellie Crome
+ */
+public class FoursquareTest {
+
+    @Test
+    public void test() {
+        assertEquals(1,1);
+    }
+
+}
