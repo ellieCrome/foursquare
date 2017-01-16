ea85f65eebd994d3ab596efa33b053cf02f1b753
Ellie Crome <ecrome@andigital.com>
2017-01-16 18:37:54 +0000

    added gradle and hello world main

---
+++ src/main/java/foursquare/foursquare.java
@@ -0,0 +1,11 @@
+package foursquare;
+
+/**
+ * Created by ecrome on 16/01/2017.
+ */
+public class foursquare {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
